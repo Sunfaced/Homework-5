228d945b344e42d0a6ded47f3fdcaea95d85b650
Alexander <akoceryzhkin@gmail.com>
2023-03-02 22:33:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// Пример 1
+
+// function buttonClick(){
+//     let elem = document.getElementById('elem')
+//     elem.innerHTML = 'Ку-ку! А я <strong>жирный</strong>!'
+// }
+
+// Пример 2
+// function buttonClick(){
+//     let elem = document.getElementById('elem')
+//     elem.outerHTML = '<h3>Абзац превратился в h3!</h3>'
+// }
+
+// Пример 3
+
+// function buttonClick(){
+//         let elem = document.getElementById('elem')
+//         elem.outerHTML = '<h3>' + elem.innerHTML + '</h3>'
+//     }
+
+// Пример 4
+
+// function buttonClick(){
+//     let elem1 = document.querySelector('#elem1').value
+//     let elem2 = document.querySelector('#elem2').value
+//     let result = document.querySelector('#result')
+//     let sum    
+//     sum = parseInt(elem1) + parseInt(elem2)
+//     result.innerHTML = sum
+// }
+
+
+// Пример 5
+
+// function buttonClick(){
+//     let elem = document.getElementsByTagName('p')
+//     elem[0].innerHTML = '0';
+//     elem[1].innerHTML = '1';
+//     elem[2].innerHTML = '2';
+//     elem[3].innerHTML = '3';
+//     elem[4].innerHTML = '4';
+//     elem[5].innerHTML = '5';
+//     elem[6].innerHTML = '6';
+//     elem[7].innerHTML = '7';
+// }
